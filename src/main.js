2e92bc7debd6151aaf1617fb8aeb876a843cfc12
Kevin <kevingm31@gmail.com>
2020-10-27 19:49:36 -0500

    Initial Commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from "axios"
+
+axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "https://damp-atoll-89484.herokuapp.com/api/questions" :
 
 Vue.config.productionTip = false
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,51 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>{{questions[0].question}}</p>
+    <form>
+      <input type="radio" name="answer" id="first" :value="questions[0].correct" v-model="choice">
+      <label for="first">{{questions[0].correct}}</label>
+      <br>
+      <input type="radio" name="answer" id="first" :value="questions[0].incorrect[0]" v-model="choice">
+      <label for="first">{{questions[0].incorrect[0]}}</label>
+      <br>
+      <input type="radio" name="answer" id="first" :value="questions[0].incorrect[1]" v-model="choice">
+      <label for="first">{{questions[0].incorrect[1]}}</label>
+      <br>
+      <input type="radio" name="answer" id="first" :value="questions[0].incorrect[2]" v-model="choice">
+      <label for="first">{{questions[0].incorrect[2]}}</label>
+      <br>
+      {{choice}}
+    </form>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import axios from 'axios'
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      questions: [],
+      choice: ""
+    }
+  },
+  methods: {
+    getQuestions: function () {
+      axios.get("https://damp-atoll-89484.herokuapp.com/api/questions")
+      .then((response) => {
+        console.log(response.data);
+        this.questions = response.data;
+      })
+    },
+    checkanswer() {
+
+    }
+  },
+  created() {
+    this.getQuestions()
   }
 }
 </script>
+
+<style>
+
+</style>
